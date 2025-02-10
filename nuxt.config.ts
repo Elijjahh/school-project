6335044e33b396edc3f05fd1dcbc7b5de0b3937e
Илья Белов <ibelov292005@gmail.com>
2025-02-11 02:20:31 +0300

    initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,53 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import ThemePreset from './theme';
+
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true },
+  ssr: false,
+  css: ['/assets/scss/main.scss'],
+  modules: [
+    '@nuxt/eslint',
+    '@vueuse/nuxt',
+    '@pinia/nuxt',
+    '@vee-validate/nuxt',
+    '@primevue/nuxt-module',
+    // '@prisma/nuxt',
+  ],
+  vite: {
+    resolve: {
+      alias: {
+        '.prisma/client/index-browser': './node_modules/.prisma/client/index-browser.js',
+      },
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          api: 'modern-compiler',
+        },
+      },
+    },
+  },
+  nitro: {
+    preset: 'bun',
+  },
+  // prisma: {
+  //   generateClient: false,
+  //   autoSetupPrisma: true,
+  //   // installStudio: false,
+  //   runMigration: false,
+  // },
+  primevue: {
+    options: {
+      theme: {
+        preset: ThemePreset,
+        options: {
+          darkModeSelector: false,
+        },
+      },
+    },
+    components: {
+      prefix: 'Prime',
+    },
+  },
+});
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup></script>
+<template>
+  <section>
+    <h1>Главная</h1>
+    это пример использования компонентов primevue
+    <form>
+      <PrimeInputText></PrimeInputText>
+      <PrimeButton>Кликни на меня</PrimeButton>
+    </form>
+  </section>
+</template>
